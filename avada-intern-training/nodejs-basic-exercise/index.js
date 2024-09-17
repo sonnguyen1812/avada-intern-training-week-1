fca32fe47df73247c9b64c461355dbe34aa4b765
Son Nguyen <son18122001@gmail.com>
2024-09-17 09:39:48 +0700

    updated

--- avada-intern-training/nodejs-basic-exercise/index.js
+++ avada-intern-training/nodejs-basic-exercise/index.js
@@ -1,36 +1,36 @@
 import fetch from 'node-fetch';
 import * as fs from 'fs';
 
-// Function to fetch all users
-async function fetchUsers() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await response.json();
-  fs.writeFileSync('./data/users.json', JSON.stringify(users, null, 2));
-  return users;
-}
+// Sử dụng async IIFE để fetch tất cả dữ liệu
+const fetchAllData = (async () => {
+  try {
+    // Gọi tất cả các API cùng lúc với Promise.all
+    const [users, posts, comments] = await Promise.all([
+      fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()),
+      fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json()),
+      fetch('https://jsonplaceholder.typicode.com/comments').then(res => res.json()),
+    ]);
 
-// Function to fetch all posts
-async function fetchPosts() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  return await response.json();
-}
+    // Lưu dữ liệu ra file
+    fs.writeFileSync('./data/users.json', JSON.stringify(users, null, 2));
+    fs.writeFileSync('./data/posts.json', JSON.stringify(posts, null, 2));
+    fs.writeFileSync('./data/comments.json', JSON.stringify(comments, null, 2));
 
-// Function to fetch all comments
-async function fetchComments() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/comments');
-  return await response.json();
-}
+    // Trả về kết quả
+    return { users, posts, comments };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+})();
 
-// Map posts and comments to users
+// Map posts và comments vào users
 async function mapPostsAndCommentsToUsers() {
-  const users = await fetchUsers();
-  const posts = await fetchPosts();
-  const comments = await fetchComments();
+  const { users, posts, comments } = await fetchAllData;
 
   const usersWithPostsAndComments = users.map(user => {
     const userPosts = posts.filter(post => post.userId === user.id);
     const userComments = comments.filter(comment =>
-      userPosts.some(post => post.id === comment.postId)
+        userPosts.some(post => post.id === comment.postId)
     );
 
     return {
@@ -74,10 +74,10 @@ async function findUserWithMostCommentsAndPosts() {
   const reformattedUsers = await reformatUsersWithCounts();
 
   const userWithMostComments = reformattedUsers.reduce((max, user) =>
-    user.commentsCount > max.commentsCount ? user : max, reformattedUsers[0]);
+      user.commentsCount > max.commentsCount ? user : max, reformattedUsers[0]);
 
   const userWithMostPosts = reformattedUsers.reduce((max, user) =>
-    user.postsCount > max.postsCount ? user : max, reformattedUsers[0]);
+      user.postsCount > max.postsCount ? user : max, reformattedUsers[0]);
 
   const result = {
     userWithMostComments,
@@ -88,7 +88,6 @@ async function findUserWithMostCommentsAndPosts() {
 
   return result;
 }
-
 
 // Sort users by postsCount descending
 async function sortUsersByPostsCount() {
@@ -101,23 +100,23 @@ async function sortUsersByPostsCount() {
 
 // Get post with ID of 1 and merge it with comments
 async function getPostWithComments(postId) {
-  const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
-  const post = await postResponse.json();
-
-  const commentsResponse = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
-  const comments = await commentsResponse.json();
+  // Gọi API cho post và comments cùng lúc
+  const [post, comments] = await Promise.all([
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`).then(res => res.json()),
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`).then(res => res.json())
+  ]);
 
   const mergedPost = {
     ...post,
-    comments: comments
+    comments
   };
 
   fs.writeFileSync('./data/post_with_comments.json', JSON.stringify(mergedPost, null, 2));
   return mergedPost;
 }
 
+// Execute all the functions
 (async () => {
-  await fetchUsers();
   await mapPostsAndCommentsToUsers();
   await filterUsersWithMoreThanThreeComments();
   await reformatUsersWithCounts();
